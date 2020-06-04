e8a4237d730741536a93416c8ccefcb26a686044
R-O-M-CODE <rafiumoshoodolakunle@gmail.com>
2020-06-04 11:07:32 +0100

    SEtting up views and database

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'AdminsController@index')->name('admin.index');
+
+Route::get('/post', 'PostController@show')->name('post');

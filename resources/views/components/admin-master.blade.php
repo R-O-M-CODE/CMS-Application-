5f5a243898f8fe845767b59ff4d07c4577aecf0a
R-O-M-CODE <rafiumoshoodolakunle@gmail.com>
2020-06-04 13:37:23 +0100

    Settin up UI for viewing posts

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -12,7 +12,6 @@
     <title>SB Admin 2 - Blank</title>
 
     <!-- Custom fonts for this template-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
@@ -379,7 +378,7 @@
 
 <!-- Custom scripts for all pages-->
 <script src="{{asset('js/sb-admin-2.js')}}"></script>
-
+@yield('scripts')
 </body>
 
 </html>
